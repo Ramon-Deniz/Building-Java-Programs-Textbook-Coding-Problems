3ad2706826b225026f5eb06fe55782c1cea9b9bb
Ramon-Deniz <ramondeniz4@gmail.com>
2017-12-23 15:27:37 -0700

    Chapter 6 Problems

---
+++ src/Main/Chapter6_Problems.java
@@ -0,0 +1,13 @@
+package Main;
+
+import java.util.*;
+
+public class Chapter6_Problems {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		char letters[] = scan.nextLine().toCharArray();
+		System.out.println(Arrays.toString(letters));
+	}
+	
+}
