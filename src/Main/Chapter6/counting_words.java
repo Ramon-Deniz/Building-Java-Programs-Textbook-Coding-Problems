1c92332458a7230f45b3b650e1279f101cd4d7ff
Ramon-Deniz <ramondeniz4@gmail.com>
2017-12-25 13:37:11 -0700

    New program added

---
+++ src/Main/Chapter6/counting_words.java
@@ -0,0 +1,45 @@
+package Main.Chapter6;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class counting_words {
+
+	public static void beginCount(File f) {
+		wordCount=0;
+		lineCount=0;
+		charCount=0;
+		try {
+			Scanner scanLine = new Scanner(f);
+			while(scanLine.hasNextLine()) {
+				Scanner scanWords = new Scanner(scanLine.nextLine());
+				while(scanWords.hasNext()) {
+					wordCount++;
+					charCount+=scanWords.next().length();
+				}
+				lineCount++;
+			}
+		}
+		
+		catch(FileNotFoundException e) {
+			System.out.println("File Not Found( count_words.beginCount() )");
+		}
+	}
+	
+	public static int getWordCount() {
+		return wordCount;
+	}
+	
+	public static int getLineCount() {
+		return lineCount;
+	}
+	
+	public static int getCharCount() {
+		return charCount;
+	}
+	
+	public static int wordCount;
+	public static int lineCount;
+	public static int charCount;
+}
--- src/Main/main.java
+++ src/Main/main.java
@@ -1,18 +1,37 @@
 package Main;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
+import Main.Chapter6.counting_words;
 import Main.Chapter6.problem_12;
 
 public class main {
 	public static void main(String args[]) {
 		// Starting point
+		
+		//----------problem_12--------------
+		/*
+		 * 
 		try {
 			problem_12.removeHTMLTags(new File("/home/miguel/eclipse-workspace/Projects/src/Main/Chapter6/htmlTags.txt"));
 			
 		} catch (NullPointerException e) {
 			System.out.println("File Not Found");
 		}
+		
+		*/
+		
+		//----------counting_words--------------
+		try {
+			counting_words.beginCount(new File("/home/miguel/eclipse-workspace/Projects/src/countingWordsDocument.txt"));
+			System.out.println("Word count: "+counting_words.getWordCount());
+			System.out.println("Line count: "+counting_words.getLineCount());
+			System.out.println("Char count: "+counting_words.getCharCount());
+		}
+		
+		catch(NullPointerException e){
+			System.out.println("File Not Found");
+		}
+		
 	}
 }
