3f569123fe9dfc9856903f5ca04bce8142e84bf4
Ramon-Deniz <ramondeniz4@gmail.com>
2018-01-11 13:46:46 -0700

    MineSweeper Modifications

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,22 +1,29 @@
 package Main;
 
-import Chapter11.Problem_2;
+import Chapter11.Problem_6;
+import Chapter11.Problem_7;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 public class main {
 
     public static void main(String args[]) {
-        ArrayList<String> words = new ArrayList<String>();
-        words.add("aa");
-        words.add("bb");
-        words.add("cc");
-        System.out.println(words);
-        LinkedList<String> words2 = new LinkedList<String>();
-        words2.add("zz");
-        words2.add("yy");
-        words2.add("xx");
-        System.out.println(words2);
-        System.out.println(Problem_2.alternate(words, words2));
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+        Random rand = new Random();
+        for(int i=0;i<rand.nextInt(25)+10;i++){
+            list.add(rand.nextInt(10)+1);
+        }
+        System.out.println(list);
+        for(int i=0;i<rand.nextInt(25)+10;i++){
+            list2.add(rand.nextInt(10)+1);
+        }
+        System.out.println(list2);
+        System.out.println();
+        System.out.println(Problem_7.countCommon(list,list2));
+        System.out.println();
+        System.out.println(Problem_6.countUnique(list));
     }
 }
